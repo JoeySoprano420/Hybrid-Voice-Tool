467e3a041b2f198c9e108ae872a15bc90ea24bf9
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2025-04-15 13:40:25 -0400

    Update VoxMystEditor.cpp

--- VoxMystEditor.cpp
+++ VoxMystEditor.cpp
@@ -23,3 +23,30 @@ loadButton.onClick = [this] {
 
 envelopeValues.resize(processor.getVocoderUnit()->getNumBands());
 startTimerHz(30); // update 30 FPS
+
+void VoxMystEditor::timerCallback() {
+    auto* unit = processor.getVocoderUnit();
+    for (int i = 0; i < envelopeValues.size(); ++i)
+        envelopeValues.set(i, unit->getEnvelopeForBand(i));
+
+    repaint(); // trigger paint() to draw visuals
+}
+
+void VoxMystEditor::paint(juce::Graphics& g) {
+    g.fillAll(juce::Colours::black);
+    
+    int width = getWidth() / envelopeValues.size();
+    for (int i = 0; i < envelopeValues.size(); ++i) {
+        float env = envelopeValues[i];
+        int barHeight = (int)(env * getHeight());
+        g.setColour(juce::Colours::cyan);
+        g.fillRect(i * width, getHeight() - barHeight, width - 2, barHeight);
+    }
+}
+
+float getEnvelopeForBand(int band) const {
+    if (band >= 0 && band < envelopes.size())
+        return envelopes[band];
+    return 0.0f;
+}
+
