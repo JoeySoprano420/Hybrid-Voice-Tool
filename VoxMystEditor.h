3cfd416077fe6d31ec340bf2e37b3260ae1fe516
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2025-04-15 13:38:38 -0400

    Update VoxMystEditor.h

--- VoxMystEditor.h
+++ VoxMystEditor.h
@@ -1,2 +1,7 @@
 juce::TextButton saveButton { "Save Preset" };
 juce::TextButton loadButton { "Load Preset" };
+
+juce::Array<float> envelopeValues;
+juce::Timer envelopeTimer;
+
+void timerCallback() override;
